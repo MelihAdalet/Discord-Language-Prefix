4e938b6d94f584698d84c048f58da49ee45a0fdd
Dev_Span <67538314+Dev-Span@users.noreply.github.com>
2022-07-10 19:35:11 +0200

    Update lang.js

--- commands/utility/lang.js
+++ commands/utility/lang.js
@@ -1,8 +1,18 @@
-const Discord = require('discord.js');
-const db = require('quick.db')
 const fs = require("fs");
+const db = require("quick.db")
+const { MessageEmbed } = require("discord.js")
+const { PREFIX } = require("../../config")
 
-exports.run = async (client, message, args, prefix) => {
+module.exports = {
+    config: {
+        name: "prefix",
+        description: "Chnage/Know The server's Prefix or the Global Prefix",
+        usage: "m/prefix <new prefix/reset>",
+        example: "1) m/prefix = \n2) m/prefix reset",
+        aliases: ["prefix"]
+    },
+
+    run: async (bot, message, args) => {
 
   let language = db.fetch(`lang_${message.guild.id}`)
   if (language === null) language = client.config.mainLang
@@ -19,23 +29,6 @@ exports.run = async (client, message, args, prefix) => {
   if (!message.member.hasPermission('ADMINISTRATOR')) return message.channel.send(mg)
   if (!args[0]) return message.channel.send(msg);
 
-
-  if (args[0] === "de") {
-    const err = new Discord.MessageEmbed()
-      .setColor("RED")
-      .setDescription(lang.lang.err)
-
-    if (language === "de") return message.channel.send(err)
-    db.set(`lang_${message.guild.id}`, "de")
-
-
-    const embed = new Discord.MessageEmbed()
-      .setColor("GREEN")
-      .setDescription(":flag_de: Die Sprache wurde geändert auf `Deutsch`")
-    message.channel.send(embed).then(messageToReact => {
-      messageToReact.react("<a:LunaYes:817452322418196535>");
-    })
-  }
 
   if (args[0] === "en") {
     const err = new Discord.MessageEmbed()
@@ -69,46 +62,5 @@ exports.run = async (client, message, args, prefix) => {
     })
   }
 
-  if (args[0] === "ru") {
-    const err = new Discord.MessageEmbed()
-      .setColor("RED")
-      .setDescription(lang.lang.err)
 
-    if (language === "ru") return message.channel.send(err)
-    db.set(`lang_${message.guild.id}`, "ru")
-
-    const embed = new Discord.MessageEmbed()
-      .setColor("GREEN")
-      .setDescription(":flag_ru: Язык изменен на `Pусский`")
-    message.channel.send(embed).then(messageToReact => {
-      messageToReact.react("<a:LunaYes:817452322418196535>");
-    })
-  }
-
-  if (args[0] === "es") {
-    const err = new Discord.MessageEmbed()
-      .setColor("RED")
-      .setDescription(lang.lang.err)
-
-    if (language === "es") return message.channel.send(err)
-    db.set(`lang_${message.guild.id}`, "es")
-
-    const embed = new Discord.MessageEmbed()
-      .setColor("GREEN")
-      .setDescription(":flag_es: El idioma fue cambiado a `Español`")
-    message.channel.send(embed).then(messageToReact => {
-      messageToReact.react("<a:LunaYes:817452322418196535>");
-    })
-  }
-}
-
-exports.conf = {
-  enabled: true,
-  guildOnly: true,
-  aliases: [],
-  permLevel: 0
-};
-
-exports.help = {
-  name: 'lang'
 };
